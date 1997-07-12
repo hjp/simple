76a7bb252fbd0b92340893f08a765ee25f0c06d5
hjp <hjp>
1997-07-12 15:17:26 +0000

    Added decimal number in output, fitted everything into < 80 chars per line again.

--- chartab/chartab.c
+++ chartab/chartab.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
+
 int main (int argc, char **argv) {
 	int i;
 	for (i = 0; i < 256; i++) {
-		printf ("%x %o %c\t", i, i, i);
-		if (i % 8 == 7) printf ("\n");
+		printf ("%x %d %o %c\t", i, i, i, i);
+		if (i % 4 == 3) printf ("\n");
 	}
 	return 0;
 }

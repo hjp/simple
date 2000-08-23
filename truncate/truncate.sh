173ccac1bc16657424fdd50f53b18ea35835e15d
hjp <hjp>
2000-08-23 13:34:52 +0000

    Now really truncate to 0 bytes (instead of 1).

--- truncate/truncate.sh
+++ truncate/truncate.sh
@@ -2,6 +2,6 @@
 @@@find_printf@@@ "$@" -type f -printf '%TY%Tm%Td%TH%TM.%TS %p\n' |
 while read timestamp file
 do
-    echo > "$file"
+    echo -n "" > "$file"
     touch -t "$timestamp" "$file"
 done

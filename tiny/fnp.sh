97152144aa0ee9f11d27d0a6c3bfaa9869410dfe
cvs2git <cvs2git>
2001-06-26 13:47:38 +0000

    This commit was manufactured by cvs2svn to create tag 'lspath-1_0'.

--- tiny/fnp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-@@@find@@@ "$@" -printf "%TY-%Tm-%Td %TH:%TM:%TS %5k %-8u %-8g %m %p\t%l\n"
--- tiny/fnpc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-@@@find@@@ "$@" -printf "%CY-%Cm-%Cd %CH:%CM:%CS %5k %-8u %-8g %m %p\t%l\n"

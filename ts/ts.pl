8327e6a6f4ac620415df79b741fd1dbdf15b8041
hjp <hjp>
2003-08-23 15:44:14 +0000

    Added microsecond resolution to perl version. Added C version.

--- ts/ts.pl
+++ ts/ts.pl
@@ -1,8 +1,19 @@
 #!@@@perl@@@ -wT
 use strict;
 use POSIX;
+use Time::HiRes qw(gettimeofday);
+my $use_microseconds;
+if ($ARGV[0] eq '-u') {
+    $use_microseconds = 1;
+    shift;
+}
 while (<>) {
     chomp;
-    my $now = strftime('%Y-%m-%dT%H:%M:%S', localtime);
-    print "$now $_\n";
+    my ($seconds, $microseconds) = gettimeofday;
+    my $now = strftime('%Y-%m-%dT%H:%M:%S', localtime($seconds));
+    if ($use_microseconds) {
+	printf "%s.%06d %s\n", $now, $microseconds, $_;
+    } else {
+	print "$now $_\n";
+    }
 }

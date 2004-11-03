af75f9b864b8e157ede38256206c929c3dd35852
hjp <hjp>
2004-11-03 14:18:39 +0000

    Added crypt.pl

---
+++ tiny/crypt.pl
@@ -0,0 +1,11 @@
+#!@@@perl@@@
+use warnings;
+use strict;
+
+unless (@ARGV == 2) {
+    print STDERR "Usage: $0 password salt\n";
+    exit(2);
+}
+my $encpwd = crypt($ARGV[0], $ARGV[1]);
+print "$encpwd\n";
+exit($ARGV[1] eq $encpwd ? 0 : 1);

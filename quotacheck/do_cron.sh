6899d0d971a8d3216d94190f790955ddfd9588ec
hjp <hjp>
2005-12-12 08:51:35 +0000

    Create graphs for inodes, too.

--- quotacheck/do_cron.sh
+++ quotacheck/do_cron.sh
@@ -2,10 +2,12 @@
 
 /usr/local/sbin/quotastat
 /usr/local/sbin/quotacheck
-find /usr/local/dfstat/quotacheck-timestamps -type f -name '*:*' -print |  sed -e 's/.*\///' -e 's/:/ /' |
-while read user mount_t
+find /usr/local/dfstat/quotacheck-timestamps -type f -name '*:*:*' -print |  sed -e 's/.*\///' -e 's/:/ /g' |
+while read user mount_t data
 do
+    g="/usr/local/dfstat/quotacheck-graphs/$user${mount_t}_$data.png"
     mount=`echo $mount_t | sed -e 's/_/\//'`
-    "/usr/local/dfstat/quotagraph" "--fs=$mount" "--user=$user" "--data=b" /usr/local/dfstat/quota.stat.????-??
-    @@@scp@@@ /usr/local/www//wsr/intranet/quotas/$user$mount_t.gif "intra.wsr.ac.at:/usr/local/www/intra/informationssysteme/quotas/$user$mount_t.gif"
+    "/usr/local/dfstat/quotagraph" "--fs=$mount" "--user=$user" "--data=$data" /usr/local/dfstat/quota.stat.????-?? > "$g"
+    @@@scp@@@ "$g" \
+	"intra.wsr.ac.at:/usr/local/www/intra/informationssysteme/quotas/$user${mount_t}_$data.png"
 done

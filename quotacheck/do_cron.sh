3836ca8c1c5a08ed83e6abaf4d312fbe1486c1e7
hjp <hjp>
2006-08-14 08:14:03 +0000

    Replace all _ with / in mountpoint.

--- quotacheck/do_cron.sh
+++ quotacheck/do_cron.sh
@@ -6,7 +6,7 @@ find /usr/local/dfstat/quotacheck-timestamps -type f -name '*:*:*' -print |  sed
 while read user mount_t data
 do
     g="/usr/local/dfstat/quotacheck-graphs/$user${mount_t}_$data.png"
-    mount=`echo $mount_t | sed -e 's/_/\//'`
+    mount=`echo $mount_t | sed -e 's/_/\//g'`
     "/usr/local/dfstat/quotagraph" "--fs=$mount" "--user=$user" "--data=$data" /usr/local/dfstat/quota.stat.????-?? > "$g"
     @@@scp@@@ "$g" \
 	"intra.wsr.ac.at:/usr/local/www/intra/informationssysteme/quotas/$user${mount_t}_$data.png"

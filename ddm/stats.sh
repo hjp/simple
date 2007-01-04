e83175a9906b629c40eb88ffc89f796f5a0279da
cvs2git <cvs2git>
2007-01-04 17:00:46 +0000

    This commit was manufactured by cvs2svn to create tag 'pathtools_1_1'.

--- ddm/stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# simple statistics (min, avg, max) for ddm output
-
-awk -F : '{ n[$4] ++; s[$4] += $3; if ($3 < min[$4]) min[$4] = $3; if ($3 > max[$4]) max[$4] = $3;  }
-END { for (i in n) { printf "%10.6f %10.6f %10.6f %s\n", min[i], s[i]/n[i], max[i], i } }' "$@" | sort -n +2
-

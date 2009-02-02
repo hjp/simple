e315472e6344f5339853da81219b027e559c9301
hjp <hjp>
2009-02-02 11:39:33 +0000

    added rules to build GNUmakerules and GNUmakevars if they are missing
    added new options:
    
     -s (seconds since the epoch)
     -r (relative to start of program)

--- ts/ts.c
+++ ts/ts.c
@@ -9,7 +9,7 @@
 static char *cmnd;
 
 static void usage(void) {
-    fprintf(stderr, "Usage: %s [-u] [-o file]\n", cmnd);
+    fprintf(stderr, "Usage: %s [-u] [-s] [-r] [-o file]\n", cmnd);
     exit(1);
 }
 
@@ -17,20 +17,30 @@ static void usage(void) {
 int main(int argc, char **argv) {
     int c;
     int use_microseconds = 0;
+    int use_seconds = 0;
     int print_ts = 1;
     char lastfile[FILENAME_MAX] = "", file[FILENAME_MAX] = "";
     char *filepat = NULL;
     FILE *fp = NULL;
+    struct timeval start = { 0, 0 };
 
     cmnd = argv[0];
 
-    while ((c = getopt(argc, argv, "uo:")) != EOF) {
+    while ((c = getopt(argc, argv, "suro:")) != EOF) {
 	switch (c) {
 	    case 'u':
 		use_microseconds = 1;
 		break;
 	    case 'o':
 		filepat = optarg;
+		break;
+	    case 's':
+		use_seconds = 1;
+		break;
+	    case 'r':
+		gettimeofday(&start, NULL);
+		use_seconds = 1;
+		use_microseconds = 1;
 		break;
 	    default:
 		usage();
@@ -64,9 +74,19 @@ int main(int argc, char **argv) {
 		}
 	    }
 
-	    strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%S",
-		     localtime(&tv.tv_sec));
-	    fputs(s, fp);
+	    if (use_seconds) {
+		tv.tv_usec -= start.tv_usec;
+		if (tv.tv_usec < 0) {
+		    tv.tv_usec += 1000000;
+		    tv.tv_sec -= 1;
+		}
+		tv.tv_sec -= start.tv_sec;
+		fprintf(fp, "%ld", (long)tv.tv_sec);
+	    } else {
+		strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%S",
+			 localtime(&tv.tv_sec));
+		fputs(s, fp);
+	    }
 	    if (use_microseconds) {
 		fprintf(fp, ".%06ld ", (long)tv.tv_usec);
 	    }

e7af75898fc82dff488837203963edf2866b46d4
hjp <hjp>
2013-10-25 09:29:12 +0000

    Diverse Anpassungen und Bugfixes

--- quotacheck/do_cron.sh
+++ quotacheck/do_cron.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+unset PERL_UNICODE
+
+ts_dir=/usr/local/dfstat/quotacheck-timestamps
+
 /usr/local/sbin/quotastat
 /usr/local/sbin/quotacheck
-find /usr/local/dfstat/quotacheck-timestamps -type f -name '*:*:*' -print |  sed -e 's/.*\///' -e 's/:/ /g' |
+
+find $ts_dir -type f -name '*:*:*' -mtime +60 -exec rm {} \;
+
+find $ts_dir -type f -name '*:*:*' -print |  sed -e 's/.*\///' -e 's/:/ /g' |
 while read user mount_t data
 do
     g="/usr/local/dfstat/quotacheck-graphs/$user${mount_t}_$data.png"
@@ -11,3 +18,4 @@ do
     @@@scp@@@ "$g" \
 	"intra.wsr.ac.at:/usr/local/www/intra/informationssysteme/quotas/$user${mount_t}_$data.png"
 done
+find /usr/local/dfstat/quotacheck-graphs/ -atime +60 -exec rm {} \;
